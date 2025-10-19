a47477df4a1365922fb6007a1ddf52dd09d758fe
Patrick Bozeman <pbozeman@gmail.com>
2025-10-19 13:57:34 -0700

    [ui] add colorscheme

--- init.lua
+++ init.lua
@@ -3,6 +3,8 @@ vim.g.mapleader = " "
 vim.g.maplocalleader = "\\"
 
 -- Load plugins
+require("plugins.colorscheme")
 require("plugins.which-key")
 require("plugins.lsp")
+require("plugins.conform")
 require("plugins.tmux")
---
+++ lua/plugins/colorscheme.lua
@@ -0,0 +1,23 @@
+-- tokyonight colorscheme configuration
+local ok, tokyonight = pcall(require, "tokyonight")
+if not ok then
+  return
+end
+
+tokyonight.setup({
+  transparent = true,
+  styles = {
+    sidebars = "transparent",
+    floats = "transparent",
+    comments = { fg = "#f8c8dc", italic = true },
+  },
+  on_colors = function(colors)
+    -- the tokyonight red has no chill, use the red from nord.
+    -- (it's a bit too pastel, but better than the default.)
+    colors.error = "#bf616a"
+    colors.red = "#bf616a"
+    colors.red1 = "#bf616a"
+  end,
+})
+
+vim.cmd.colorscheme("tokyonight")
