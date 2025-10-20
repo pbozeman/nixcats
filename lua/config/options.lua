4cb34de331e0f8aec0818f39f98274c27c580c85
Patrick Bozeman <pbozeman@gmail.com>
2025-10-20 02:32:57 -0700

    [opts] shrink cmd line

--- lua/config/options.lua
+++ lua/config/options.lua
@@ -16,3 +16,6 @@ opt.cursorline = true
 -- Clipboard
 -- Use OSC 52 for consistent clipboard behavior locally and over SSH
 opt.clipboard = ""
+
+-- Don't show cmd line when not in use
+opt.cmdheight = 0
