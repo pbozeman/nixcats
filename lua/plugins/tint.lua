454d3832a9da65c681b6ea854e1ddb5a2bf066e4
Patrick Bozeman <pbozeman@gmail.com>
2025-10-20 22:31:15 -0700

    [ui][tint] tweak tints

--- lua/plugins/tint.lua
+++ lua/plugins/tint.lua
@@ -7,7 +7,7 @@ tint.setup({
   -- Darken colors, use a positive value to brighten
   tint = -50,
   -- Saturation to preserve
-  saturation = 0.6,
+  saturation = 0.8,
   -- Showing default behavior, but value here can be predefined set of transforms
   transforms = require("tint").transforms.SATURATE_TINT,
   -- Tint background portions of highlight groups
