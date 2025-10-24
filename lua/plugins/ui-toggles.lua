7d3cdf16ffd0c9fe0d01d16bf5c75a2c76047783
Patrick Bozeman <pbozeman@gmail.com>
2025-10-24 10:20:55 -0700

    [ui] use snacks toggle

--- lua/plugins/ui-toggles.lua
+++ lua/plugins/ui-toggles.lua
@@ -1,65 +1,96 @@
--- UI toggle functions similar to LazyVim
+-- UI toggle functions using Snacks (matching LazyVim)
 
-local snacks_ok, snacks = pcall(require, "snacks")
+local ok, Snacks = pcall(require, "snacks")
+if not ok then
+  return
+end
 
 -- Toggle diagnostics
-vim.keymap.set("n", "<leader>ud", function()
-  vim.diagnostic.enable(not vim.diagnostic.is_enabled())
-end, { desc = "Toggle Diagnostics" })
+Snacks.toggle({
+  name = "Diagnostics",
+  get = vim.diagnostic.is_enabled,
+  set = vim.diagnostic.enable,
+}):map("<leader>ud")
 
 -- Toggle line numbers
-vim.keymap.set("n", "<leader>ul", function()
-  vim.opt.number = not vim.o.number
-end, { desc = "Toggle Line Numbers" })
+Snacks.toggle({
+  name = "Line Numbers",
+  get = function()
+    return vim.opt.number:get()
+  end,
+  set = function(state)
+    vim.opt.number = state
+  end,
+}):map("<leader>ul")
 
 -- Toggle relative line numbers
-vim.keymap.set("n", "<leader>uL", function()
-  vim.opt.relativenumber = not vim.o.relativenumber
-end, { desc = "Toggle Relative Numbers" })
+Snacks.toggle({
+  name = "Relative Numbers",
+  get = function()
+    return vim.opt.relativenumber:get()
+  end,
+  set = function(state)
+    vim.opt.relativenumber = state
+  end,
+}):map("<leader>uL")
 
 -- Toggle spell checking
-vim.keymap.set("n", "<leader>us", function()
-  vim.opt.spell = not vim.o.spell
-end, { desc = "Toggle Spelling" })
+Snacks.toggle({
+  name = "Spelling",
+  get = function()
+    return vim.opt.spell:get()
+  end,
+  set = function(state)
+    vim.opt.spell = state
+  end,
+}):map("<leader>us")
 
 -- Toggle line wrap
-vim.keymap.set("n", "<leader>uw", function()
-  vim.opt.wrap = not vim.o.wrap
-end, { desc = "Toggle Line Wrap" })
+Snacks.toggle({
+  name = "Line Wrap",
+  get = function()
+    return vim.opt.wrap:get()
+  end,
+  set = function(state)
+    vim.opt.wrap = state
+  end,
+}):map("<leader>uw")
 
 -- Toggle inlay hints
-vim.keymap.set("n", "<leader>uh", function()
-  vim.lsp.inlay_hint.enable(not vim.lsp.inlay_hint.is_enabled())
-end, { desc = "Toggle Inlay Hints" })
+Snacks.toggle({
+  name = "Inlay Hints",
+  get = function()
+    return vim.lsp.inlay_hint.is_enabled({ bufnr = 0 })
+  end,
+  set = function(state)
+    vim.lsp.inlay_hint.enable(state)
+  end,
+}):map("<leader>uh")
 
 -- Toggle conceallevel
-local conceallevel = vim.o.conceallevel
-vim.keymap.set("n", "<leader>uc", function()
-  if vim.o.conceallevel == 0 then
-    vim.opt.conceallevel = conceallevel
-  else
-    conceallevel = vim.o.conceallevel
-    vim.opt.conceallevel = 0
-  end
-end, { desc = "Toggle Conceal" })
+Snacks.toggle({
+  name = "Conceal",
+  get = function()
+    return vim.opt.conceallevel:get() > 0
+  end,
+  set = function(state)
+    vim.opt.conceallevel = state and 2 or 0
+  end,
+}):map("<leader>uc")
 
 -- Toggle auto-completion (blink.cmp)
 -- When disabled, you can still manually trigger with <C-Space>
-if snacks_ok then
-  vim.g.auto_completion_enabled = false
-  snacks
-    .toggle({
-      name = "Auto-Completion",
-      notify = false,
-      get = function()
-        return vim.g.auto_completion_enabled
-      end,
-      set = function(state)
-        vim.g.auto_completion_enabled = state
-        if not state then
-          require("blink.cmp").hide()
-        end
-      end,
-    })
-    :map("<leader>ua")
-end
+vim.g.auto_completion_enabled = false
+Snacks.toggle({
+  name = "Auto-Completion",
+  notify = false,
+  get = function()
+    return vim.g.auto_completion_enabled
+  end,
+  set = function(state)
+    vim.g.auto_completion_enabled = state
+    if not state then
+      require("blink.cmp").hide()
+    end
+  end,
+}):map("<leader>ua")
